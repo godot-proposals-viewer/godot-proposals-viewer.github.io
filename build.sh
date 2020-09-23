9fc8f05c677266ada8c0fba170d8ca917c88093d
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-09-23 18:30:07 +0200

    Update the number of proposals to fetch
    There are over 1,100 open proposals now, so we need 12 requests.

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ echo "Cleaning previous JSON proposal data..."
 rm -rf "proposals"
 mkdir -p "proposals"
 
-for i in {1..10}; do
-  echo "Requesting batch of proposals $i/10..."
+for i in {1..12}; do
+  echo "Requesting batch of proposals $i/12..."
   curl -fSs \
       -H "Accept: application/vnd.github.squirrel-girl-preview" \
       "https://api.github.com/repos/godotengine/godot-proposals/issues?state=open&per_page=100&page=$i" \
